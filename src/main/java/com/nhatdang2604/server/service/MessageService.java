2c08cbcf5417ddb8018804d752f429f7f05ac0f5
nhatdang2604 <nhatdang2604@gmail.com>
2022-05-21 10:25:57 +0700

    Add recieve method + fix send method in MessageService

--- src/main/java/com/nhatdang2604/server/service/MessageService.java
+++ src/main/java/com/nhatdang2604/server/service/MessageService.java
@@ -1,7 +1,14 @@
 package com.nhatdang2604.server.service;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 import com.nhatdang2604.server.dao.MessageDAO;
+import com.nhatdang2604.server.model.entities.Client;
 import com.nhatdang2604.server.model.entities.Message;
+import com.nhatdang2604.server.model.entities.Room;
 
 public enum MessageService {
 
@@ -16,8 +23,49 @@ public enum MessageService {
 		messageDAO = MessageDAO.INSTANCE;
 	}
 
-	public Message send(Message message) {
-		return messageDAO.createMessage(message);
+	//Recieve a message from a client
+	public Message recieve(Client client) {
+		
+		Socket socket = client.getSocket();
+		Message message = null;
+		try {
+			ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
+			try {
+				message = (Message) inStream.readObject();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return message;
+	}
+	
+	//Send a message, and inject the client, room into the message
+	public Message send(Message message, Client client, Room room) {
+		
+		Socket socket = client.getSocket();
+		
+		//Inject attributes for the message
+		message.setClient(client);
+		message.setRoom(room);
+		
+		ObjectOutputStream output;
+		try {
+			output = new ObjectOutputStream(socket.getOutputStream());
+			output.writeObject(message);
+		    output.flush();
+		    message = messageDAO.createMessage(message);
+		    
+		} catch (IOException e) {
+			e.printStackTrace();
+			message = null;
+		}
+       
+		
+		return message;
 	}
 	
 }
